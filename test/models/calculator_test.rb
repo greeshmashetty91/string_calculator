c97cc2d5a13c684a3b19d71257a9dcc2bff20f3f
greeshmashetty91 <Greeshma.Shetty@cerner.com>
2024-03-21 16:56:29 +0530

    updating test string

--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -21,7 +21,7 @@ class CalculatorTest < ActiveSupport::TestCase
     assert_equal 6, Calculator.add("1\n2,3")
   end
 
-  test "input with invalid format raises error" do
+  test "input with invalid format return error message" do
     assert_equal "Numbers value is invalid", Calculator.add("1,\n")
   end
 end
